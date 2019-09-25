e2428fa03f84901fcd20395ddd46e2981ee91453
danielletaynara <santos_silva1997@hotmail.com>
2019-09-25 15:45:18 -0300

    documentação atualizar laboratorio

--- start/routes.js
+++ start/routes.js
@@ -26,6 +26,6 @@ Route.get("/software/:id", "SoftwareController.show");
 Route.put("/software/:id", "SoftwareController.update");
 Route.delete("/software/:id", "SoftwareController.destroy");
 
-Route.get("/laboratories", "LaboratoryController.index");
+Route.get("/laboratory", "LaboratoryController.index");
 Route.post("/laboratory", "LaboratoryController.store");
 Route.put("/laboradory/:id", "LaboratoryController.update")
