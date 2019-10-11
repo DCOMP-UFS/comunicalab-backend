fa4af1817f4d4337da4f322eeb50615c658e7e44
Rivanildo Júnior <rivanildo_junior7@hotmail.com>
2019-10-10 22:58:35 -0300

    retirado codigo estranho

--- app/Controllers/Http/CalledController.js
+++ app/Controllers/Http/CalledController.js
@@ -1,5 +1,4 @@
-
-'use strict'
+"use strict";
 
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @typedef {import('@adonisjs/framework/src/Response')} Response */
@@ -20,7 +19,7 @@ class CalledController {
    * @param {Response} ctx.response
    * @param {View} ctx.view
    */
-  async index ({ request, response, view }) {
+  async index({ request, response, view }) {
     try {
       const data = await Called.query()
         .where("isDeleted", false)
@@ -35,7 +34,7 @@ class CalledController {
    * Create/save a new called.
    * POST calleds
    */
-  async store ({ request, response }) {
+  async store({ request, response }) {
     try {
       const data = request.only(["description", "type", "equipment_id"]);
       data.isDeleted = false;
@@ -47,11 +46,11 @@ class CalledController {
     }
   }
 
-    /**
+  /**
    * Display a single called.
    * GET calleds/:id
    */
-  async show ({ params, request, response, view }) {
+  async show({ params, request, response, view }) {
     try {
       const called = await Called.query()
         .where("id", params.id)
@@ -70,11 +69,11 @@ class CalledController {
     }
   }
 
-    /**
+  /**
    * Update called details.
    * PUT or PATCH calleds/:id
    */
-  async update ({ params, request, response }) {
+  async update({ params, request, response }) {
     try {
       const data = request.post();
 
@@ -99,11 +98,11 @@ class CalledController {
    * Delete a called with id.
    * DELETE calleds/:id
    */
-  async destroy ({ params, request, response }) {
+  async destroy({ params, request, response }) {
     try {
       const called = await Called.query()
         .where("id", params.id)
-        .update({isDeleted: true});
+        .update({ isDeleted: true });
 
       if (called === 0) {
         return response.status(404).send({ message: "Not Found" });
@@ -116,8 +115,6 @@ class CalledController {
       return response.status(error.status).send({ message: error });
     }
   }
-  n
 }
 
-module.exports = CalledController
-
+module.exports = CalledController;
