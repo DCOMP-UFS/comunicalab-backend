ea0938b39df403024332edfc05230edd3dcd2007
Marcel <marcel.aragao@dcomp.ufs.br>
2019-10-14 00:01:09 -0300

    ajustes nas validacoes

---
+++ app/Validators/Equipment.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const Antl = use("Antl");
+
+class Equipment {
+  get validateAll() {
+    return true;
+  }
+
+  get rules() {
+    return {
+      brand: "string|max:100",
+      isDeleted: "boolean",
+      equipCategory_id: "exists:equip_categories,id",
+      laboratory_id: "exists:laboratories,id",
+      specification_id: "exists:specifications,id"
+    };
+  }
+
+  get messages() {
+    return Antl.list("validation");
+  }
+}
+
+module.exports = Equipment;
