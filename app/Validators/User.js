1fbea27da4f38cc7c26580ca74a6fdcdb2ee8c64
marcelvini <marcel.aragao@dcomp.ufs.br>
2020-01-18 14:31:00 -0300

    conserta documentação e erros de validação

--- app/Validators/User.js
+++ app/Validators/User.js
@@ -7,10 +7,10 @@ class Software {
 
   get rules() {
     return {
-      username: 'string|max:100',
-      affiliate_id: 'exists:affiliation,id',
-      email: 'string|max:100',
-      password: 'string|max:45',
+      username: 'string|max:100|unique:users, username',
+      affiliation_id: 'exists:affiliations,id',
+      email: 'string|max:100|unique:users,email',
+      password: 'string|min:6',
       is_deleted: 'boolean',
     };
   }
