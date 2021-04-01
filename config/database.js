d0e329a2a1c0e577f42cdc06e6a4639dc383dff8
tulasimaharani <tulasimaharani15@gmail.com>
2021-03-31 21:04:45 -0300

    Correct ssl variable value

--- config/database.js
+++ config/database.js
@@ -73,17 +73,12 @@ module.exports = {
   pg: {
     client: 'pg',
     connection: {
-      host: Env.get('DB_HOST', 'localhost'),
+      host: Env.get('DB_HOST', ''),
       port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', 'root'),
+      user: Env.get('DB_USER', ''),
       password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis'),
-      ssl: true,
-      extra: {
-        ssl: {
-          rejectUnauthorized: false,
-        },
-      },
+      database: Env.get('DB_DATABASE', ''),
+      ssl: { rejectUnauthorized: false },
     },
     debug: Env.get('DB_DEBUG', false),
   },
